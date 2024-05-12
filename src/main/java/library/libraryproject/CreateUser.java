0ac3ee8ac11ae5c22fb316e71d6268ec87e12427
San05M <san.mo.dra5@gmail.com>
2024-05-12 13:37:15 +0200

    Login Menu and Create add

---
+++ src/main/java/library/libraryproject/CreateUser.java
@@ -0,0 +1,18 @@
+package library.libraryproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateUser {
+    @FXML
+    private Button bottomExitCreate;
+    @FXML
+    private TextField textCreateUser;
+    @FXML
+    private TextField textCreatePassword;
+    @FXML
+    private TextField textCreateId;
+    @FXML
+    private Button bottonCreateAcount;
+}
---
+++ src/main/java/library/libraryproject/Menu.java
@@ -0,0 +1,17 @@
+package library.libraryproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class Menu {
+    @FXML
+    private Button bottomViewUsers;
+    @FXML
+    private Button bottomViewInventory;
+    @FXML
+    private Button bottomViewLoans;
+    @FXML
+    private Button bottomCreateUser;
+    @FXML
+    private Button bottomExit;
+}
