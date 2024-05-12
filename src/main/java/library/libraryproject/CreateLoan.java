4966c29daab1b244841031862b387a62a38243d4
San05M <san.mo.dra5@gmail.com>
2024-05-12 22:44:33 +0200

    Create Loan add

---
+++ src/main/java/library/libraryproject/CreateLoan.java
@@ -0,0 +1,20 @@
+package library.libraryproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class CreateLoan {
+    @FXML
+    private Button buttonExitCreateLoan;
+    @FXML
+    private Button buttonCreateCreateLoan;
+    @FXML
+    private TextField textUserIdCreateLoan;
+    @FXML
+    private TextField textBookCreateLoan;
+    @FXML
+    private TextField textLoanCreateLoan;
+    @FXML
+    private TextField textCheckCreateLoan;
+}
