5b9fd131e036b7825c46fd6a1c7c32cf794e8d0f
San05M <san.mo.dra5@gmail.com>
2024-05-12 22:35:15 +0200

    Create Book add

--- src/main/java/library/libraryproject/CreateBook.java
+++ src/main/java/library/libraryproject/CreateBook.java
@@ -1,4 +1,18 @@
 package library.libraryproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CreateBook {
+    @FXML
+    private Button buttonExitCreateBook;
+    @FXML
+    private TextField textNameCreateBook;
+    @FXML
+    private TextField textAuthorCreateBook;
+    @FXML
+    private TextField textGenreCreateBook;
+    @FXML
+    private Button buttonCreateCreateBook;
 }
