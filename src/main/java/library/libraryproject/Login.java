3d1939b2ed8225a292e15db6345e329c2f8e0acb
San05M <san.mo.dra5@gmail.com>
2024-05-12 11:08:51 +0200

    Login add

---
+++ src/main/java/library/libraryproject/Login.java
@@ -0,0 +1,14 @@
+package library.libraryproject;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Login {
+    @FXML
+    private TextField textUserNameId;
+    @FXML
+    private TextField textPassword;
+    @FXML
+    private Button bottomAccessLogin;
+}
