bbdaf4b654b2bb464b002332aa40722e22dc3cfe
San05M <san.mo.dra5@gmail.com>
2024-05-13 14:05:31 +0200

    ListUsers id created

--- src/main/java/library/libraryproject/ListUsers.java
+++ src/main/java/library/libraryproject/ListUsers.java
@@ -1,4 +1,29 @@
 package library.libraryproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class ListUsers {
+    @FXML
+    private Button buttonListUsersExit;
+    @FXML
+    private Button buttonListUsersSearch;
+    @FXML
+    private TextField textListUsersId;
+    @FXML
+    private TextField textListUsersName;
+    @FXML
+    private Button buttonListUserAddUser;
+    @FXML
+    private TableColumn columnListUsersId;
+    @FXML
+    private TableColumn columnListUserName;
+    @FXML
+    private TableColumn columnListUsersEmail;
+    @FXML
+    private TableColumn columnListUserPhoneNumber;
+    @FXML
+    private TableColumn columnListUserRemove;
 }
