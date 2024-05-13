589d0eb29bd67fa61c3a240698908589c3c78c5c
San05M <san.mo.dra5@gmail.com>
2024-05-13 14:12:50 +0200

    ListBooks id created

--- src/main/java/library/libraryproject/ListBooks.java
+++ src/main/java/library/libraryproject/ListBooks.java
@@ -1,4 +1,33 @@
 package library.libraryproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class ListBooks {
+    @FXML
+    private Button ButtonListBookExit;
+    @FXML
+    private Button ButtonListBookSearch;
+    @FXML
+    private TextField textListBookId;
+    @FXML
+    private TextField textListBookName;
+    @FXML
+    private TextField textListBookAuthor;
+    @FXML
+    private TextField textListBookGenre;
+    @FXML
+    private Button buttonListBookAddBook;
+    @FXML
+    private TableColumn columnListBookId;
+    @FXML
+    private TableColumn columnListBookName;
+    @FXML
+    private TableColumn columnListBookAuthor;
+    @FXML
+    private TableColumn columnListBookGenre;
+    @FXML
+    private TableColumn columnListBookRemove;
 }
