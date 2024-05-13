8a73215da3082cbc7adcdd1ccb54318f9e53ba26
San05M <san.mo.dra5@gmail.com>
2024-05-13 14:25:16 +0200

    ListLoans id created

--- src/main/java/library/libraryproject/ListLoans.java
+++ src/main/java/library/libraryproject/ListLoans.java
@@ -1,4 +1,35 @@
 package library.libraryproject;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class ListLoans {
+    @FXML
+    private Button buttonListLoansAddExit;
+    @FXML
+    private Button buttonListLoansSearch;
+    @FXML
+    private TextField textListLoansName;
+    @FXML
+    private TextField textListLoansBook;
+    @FXML
+    private TextField textListLoansDataLoan;
+    @FXML
+    private TextField textListLoansDataCheck;
+    @FXML
+    private Button buttonListLoansAddLoan;
+    @FXML
+    private TableColumn columnListLoansName;
+    @FXML
+    private TableColumn columnListLoansBookName;
+    @FXML
+    private TableColumn columnListLoansDataLoan;
+    @FXML
+    private TableColumn columnListLoansDataCheck;
+    @FXML
+    private TableColumn columnListLoansContact;
+    @FXML
+    private TableColumn columnListLoansRemove;
 }
