bbe47263b9c4a4e403a7e6c1a3f863f90c2e046c
San05M <san.mo.dra5@gmail.com>
2024-05-24 18:19:02 +0200

    Add buttoms to CreateWorker

--- src/main/java/library/libraryproject/CreateWorker.java
+++ src/main/java/library/libraryproject/CreateWorker.java
@@ -1,8 +1,13 @@
 package library.libraryproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateWorker {
     @FXML
@@ -15,4 +20,14 @@ public class CreateWorker {
     private TextField textCreateId;
     @FXML
     private Button bottonCreateAcount;
+
+    public void goToMenuCreate(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("menu.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
+
+    public void goToMenuExit(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("menu.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
 }
