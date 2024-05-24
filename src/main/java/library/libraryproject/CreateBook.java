cde91344a541a0d01f64a18cb4b9199f8261da33
San05M <san.mo.dra5@gmail.com>
2024-05-24 18:19:23 +0200

    Add buttoms to CreateBook

--- src/main/java/library/libraryproject/CreateBook.java
+++ src/main/java/library/libraryproject/CreateBook.java
@@ -1,8 +1,13 @@
 package library.libraryproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateBook {
     @FXML
@@ -15,4 +20,14 @@ public class CreateBook {
     private TextField textGenreCreateBook;
     @FXML
     private Button buttonCreateCreateBook;
+
+    public void goToListBook(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("ListBook.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
+
+    public void goToMenu(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("menu.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
 }
