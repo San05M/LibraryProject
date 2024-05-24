3d91a619874b2280ed1a1085bd32cae81505d010
San05M <san.mo.dra5@gmail.com>
2024-05-24 18:05:29 +0200

    Add the buttoms of Users

--- src/main/java/library/libraryproject/ListUsers.java
+++ src/main/java/library/libraryproject/ListUsers.java
@@ -1,9 +1,14 @@
 package library.libraryproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ListUsers {
     @FXML
@@ -26,4 +31,14 @@ public class ListUsers {
     private TableColumn columnListUserPhoneNumber;
     @FXML
     private TableColumn columnListUserRemove;
+
+    public void goToMenu(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("menu.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
+
+    public void goToCreateUser(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("CreateUser.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
 }
