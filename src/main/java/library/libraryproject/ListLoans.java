61e8412b8bc47a410e6d31aebc4a24940aaba7af
San05M <san.mo.dra5@gmail.com>
2024-05-24 18:05:20 +0200

    Add the buttoms of Loans

--- src/main/java/library/libraryproject/ListLoans.java
+++ src/main/java/library/libraryproject/ListLoans.java
@@ -1,9 +1,14 @@
 package library.libraryproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ListLoans {
     @FXML
@@ -32,4 +37,14 @@ public class ListLoans {
     private TableColumn columnListLoansContact;
     @FXML
     private TableColumn columnListLoansRemove;
+
+    public void goToCreateLoan(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("CreateLoan.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
+
+    public void goToMenu(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("menu.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
 }
