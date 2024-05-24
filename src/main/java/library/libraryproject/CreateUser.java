ac9f45a816d2cdcb98ecfe7f39a1fff681fa7a75
San05M <san.mo.dra5@gmail.com>
2024-05-24 18:19:10 +0200

    Add buttoms to CreateUser

--- src/main/java/library/libraryproject/CreateUser.java
+++ src/main/java/library/libraryproject/CreateUser.java
@@ -1,8 +1,13 @@
 package library.libraryproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateUser {
     @FXML
@@ -17,4 +22,14 @@ public class CreateUser {
     private TextField textPhoneNumberCreateUser;
     @FXML
     private Button ButtonCreateCreateUsser;
+
+    public void goToMenu(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("menu.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
+
+    public void goToListUsers(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("ListUsers.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
 }
