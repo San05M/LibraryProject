cfe92faf6be25c5bd44aee48a1092c98c0c71ebd
San05M <san.mo.dra5@gmail.com>
2024-05-24 18:19:16 +0200

    Add buttoms to CreateLoan

--- src/main/java/library/libraryproject/CreateLoan.java
+++ src/main/java/library/libraryproject/CreateLoan.java
@@ -1,8 +1,13 @@
 package library.libraryproject;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CreateLoan {
     @FXML
@@ -17,4 +22,14 @@ public class CreateLoan {
     private TextField textLoanCreateLoan;
     @FXML
     private TextField textCheckCreateLoan;
+
+    public void goToListLoan(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("ListLoan.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
+
+    public void goToMenu(ActionEvent actionEvent) throws IOException {
+        SceneLoader.loadScreen("menu.fxml",
+                (Stage)((Node) actionEvent.getSource()).getScene().getWindow());
+    }
 }
