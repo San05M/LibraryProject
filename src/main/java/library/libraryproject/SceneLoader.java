5312e4811f9f6aceb376dbe94d82c5bc5695e900
San05M <san.mo.dra5@gmail.com>
2024-05-26 16:06:50 +0200

    Javadoc SceneLoader

--- src/main/java/library/libraryproject/SceneLoader.java
+++ src/main/java/library/libraryproject/SceneLoader.java
@@ -7,8 +7,21 @@ import javafx.scene.control.Alert;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
+/**
+ * Utility class for loading FXML views and alert dialogs.
+ * @author sandramoyaortega
+ * @version 2
+ * @since 1
+ */
 
 public class SceneLoader {
+    /**
+     * Loads the specified FXML view.
+     *
+     * @param viewPath The path to the FXML file representing the view.
+     * @param stage    The stage where the view will be loaded.
+     */
+
     public static void loadScreen(String viewPath, Stage stage)
             throws IOException {
         Parent root = FXMLLoader.load(
@@ -18,6 +31,11 @@ public class SceneLoader {
         stage.show();
     }
 
+    /**
+     * Displays alert dialog with the error message.
+     *
+     * @param text The error message.
+     */
     public static void alertSpam(String text){
         Alert dialog = new Alert(Alert.AlertType.ERROR);
         dialog.setTitle("Error");
